8c62b93397334f88cffcdcfc4c4c4e7e0d46df21
Paul Kilfoil <paulkilfoil@outlook.com>
2020-05-28 10:41:13 +0200

    Add files via upload

--- PCIWebFinAid/JS/Timeout.js
+++ PCIWebFinAid/JS/Timeout.js
@@ -18,8 +18,6 @@ catch (x)
 
 function CheckSessionTime()
 {
-//	GetElt("lblTime").innerText = sessionTime;
-
 	var dt1;
 	var dt2;
 
@@ -30,8 +28,7 @@ function CheckSessionTime()
 		if (sessionTime <= 0)
 		{
 			alert("We're sorry, but you took too long. Your session has EXPIRED.");
-			location.href = "Identify.aspx?ErrNo=9187362";
-		//	location.href = "Login.aspx?ErrNo=9187362";
+			location.href = "XLogin.aspx?ErrNo=104317";
 			return;
 		}
 		dt1 = new Date();
