421fd2d579d0c4b84dbdccf291c9545ccef5100d
Paul Kilfoil <paulkilfoil@outlook.com>
2020-06-26 13:22:33 +0200

    Add files via upload

--- PCIBusiness/SystemDetails.cs
+++ PCIBusiness/SystemDetails.cs
@@ -5,8 +5,8 @@
 	//	Now kept in Web.AppDetails ...
 	//	public static string AppName    = "Prosperian Capital Recurring Payments";
 
-		public static string AppVersion = "1.2.015"; // "1.1.053";
-		public static string AppDate    = "2020/06/26 10:33:29";
+		public static string AppVersion = "1.2.016"; // "1.1.053";
+		public static string AppDate    = "2020/06/26 13:01:44";
 		public static string Developer  = "Paul Kilfoil, Software Development & IT Consulting";
 		public static string Owner      = "Prosperian Capital International";
 		public static string OwnerPhone = "+27 21 950 6060";
---
+++ PCIBusiness/TickerState.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace PCIBusiness
+{
+	public class TickerState : BaseData
+	{
+		private string    dbConnection;
+		private string    tickerCode;
+		private string    tickerStatus;
+		private string    userCode;
+		private string    origin;
+		private DateTime  dateUpdated;
+
+		public  string    DBConnection
+		{
+			get { return   Tools.NullToString(dbConnection); }
+			set { dbConnection = value.Trim(); }
+		}
+		public  string    TickerCode
+		{
+			get { return   Tools.NullToString(tickerCode); }
+			set { tickerCode = value.Trim(); }
+		}
+		public  string    TickerStatus
+		{
+			get { return   Tools.NullToString(tickerStatus); }
+			set { tickerStatus = value.Trim(); }
+		}
+		public  string    UserCode
+		{
+			get { return   Tools.NullToString(userCode); }
+			set { userCode = value.Trim(); }
+		}
+		public  string    Origin
+		{
+			get { return   Tools.NullToString(origin); }
+			set { origin = value.Trim(); }
+		}
+
+		private int DoSQL()
+		{
+			try
+			{
+				Tools.LogInfo("TickerState.DoSQL/1",sql,222);
+				int ret = ExecuteSQL(null,1,false,DBConnection);
+				if ( ret == 0 )
+					LoadData(dbConn);
+				else
+					Tools.LogInfo("TickerState.DoSQL/2","Error, ret="+ret.ToString(),222);
+				return ret;
+			}
+			catch (Exception ex)
+			{
+				Tools.LogInfo     ("TickerState.DoSQL/3",ex.Message,222);
+				Tools.LogException("TickerState.DoSQL/4",sql,ex);
+			}
+			finally
+			{
+				Tools.CloseDB(ref dbConn);
+			}
+			return 205;
+		}
+
+		public int Update()
+		{
+			if ( TickerStatus.Length < 1 )
+				return 103;
+
+			sql = "exec sp_TickerState  @TickerStatus=" + Tools.DBString(TickerStatus)
+			                        + ",@TickerCode="   + Tools.DBString(TickerCode)
+			                        + ",@UserCode="     + Tools.DBString(UserCode)
+			                        + ",@ActionOrigin=" + Tools.DBString(Origin);
+			return DoSQL();
+		}
+
+		public int Enquire()
+		{
+			sql = "exec sp_TickerState";
+			return DoSQL();
+		}
+
+		public override void LoadData(DBConn dbConn)
+		{
+			dbConn.SourceInfo = "TickerState.LoadData";
+			tickerCode        = dbConn.ColString("TickerCode");
+			tickerStatus      = dbConn.ColString("TickerStatus");
+			userCode          = dbConn.ColString("UserCode");
+			origin            = dbConn.ColString("ActionOrigin");
+		}
+	}
+}
