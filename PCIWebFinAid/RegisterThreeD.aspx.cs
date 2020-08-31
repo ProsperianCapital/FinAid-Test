b6f25b9f525ccb01ec900d06772cc989a48e8cda
Paul Kilfoil <paulkilfoil@outlook.com>
2020-08-31 16:47:08 +0200

    Add files via upload

--- PCIWebFinAid/RegisterThreeD.aspx.cs
+++ PCIWebFinAid/RegisterThreeD.aspx.cs
@@ -18,51 +18,76 @@ namespace PCIWebFinAid
 				                                              + ",@ReferenceNumber=" + Tools.DBString(peachID)
 				                                              + ",@Status=";
 
-			using (TransactionPeach trans = new TransactionPeach())
+			try
 			{
-				ret       = trans.ThreeDSecureCheck(peachID);
-				peachCode = Tools.DBString(trans.ResultCode);
-				sql       = sql + Tools.DBString(peachCode);
-			}
+				using (TransactionPeach trans = new TransactionPeach())
+				{
+					ret       = trans.ThreeDSecureCheck(peachID);
+					peachCode = Tools.DBString(trans.ResultCode);
+					sql       = sql + Tools.DBString(peachCode);
+				}
 
-			using (MiscList mList = new MiscList())
-			{
+				Tools.LogInfo("RegisterThreeD.PageLoad/5",sql,222);
+
+				using (MiscList mList = new MiscList())
+				{
 //	Single language
-//				ret = mList.ExecQuery(sql,0,"",false,true);
+//					ret = mList.ExecQuery(sql,0,"",false,true);
 
 //	Multi language
-				if ( mList.ExecQuery(sql,0) == 0 )
-				{
-					string  fieldCode;
-					Literal ctlLabel;
-					sql = "exec sp_WP_Get_ProductWebsiteRegContent"
-					    +     " @ProductCode="         + Tools.DBString(mList.GetColumn("ProductCode"))
-						 +     ",@LanguageCode="        + Tools.DBString(mList.GetColumn("LanguageCode"))
-						 +     ",@LanguageDialectCode=" + Tools.DBString(mList.GetColumn("LanguageDialectCode"))
-						 +     ",@Page='S'";
 					ret = mList.ExecQuery(sql,0);
-					if ( ret == 0 && ! mList.EOF )
-						while ( ! mList.EOF )
+					if ( ret == 0 )
+					{
+						string  pc  = mList.GetColumn("ProductCode");
+						string  lc  = mList.GetColumn("LanguageCode");
+						string  ldc = mList.GetColumn("LanguageDialectCode");
+						string  fieldCode;
+						string  fieldValue;
+						Literal ctlLabel;
+						sql = "exec sp_WP_Get_ProductWebsiteRegContent"
+						    +     " @ProductCode="         + Tools.DBString(pc)
+							 +     ",@LanguageCode="        + Tools.DBString(lc)
+							 +     ",@LanguageDialectCode=" + Tools.DBString(ldc)
+							 +     ",@Page='S'";
+
+						Tools.LogInfo("RegisterThreeD.PageLoad/10",sql,222);
+
+						ret = mList.ExecQuery(sql,0);
+						if ( ret == 0 && ! mList.EOF )
+							while ( ! mList.EOF )
+							{
+								fieldCode  = mList.GetColumn("WebsiteFieldCode");
+								fieldValue = mList.GetColumn("WebsiteFieldValue");
+								Tools.LogInfo("RegisterThreeD.PageLoad/15","WebsiteFieldCode="+fieldCode+" ("+fieldValue+")",222);
+								ctlLabel  = (Literal)FindControl("lbl"+fieldCode);
+								if ( ctlLabel   != null )
+									ctlLabel.Text = fieldValue.Replace(Environment.NewLine,"<br />");
+								mList.NextRow();
+							}
+						else
 						{
-							fieldCode = mList.GetColumn("WebsiteFieldCode");
-							ctlLabel  = (Literal)FindControl("lbl"+fieldCode);
-							if ( ctlLabel   != null )
-								ctlLabel.Text = mList.GetColumn("WebsiteFieldValue").Replace(Environment.NewLine,"<br />");
-							mList.NextRow();
+							lbl100503.Text = "Oops ...";
+							lbl100504.Text = "Something seems to have gone wrong with your payment.<br /><br />We have logged the error and will investigate";
+							sql            =   "Contract Code="     + transRef
+							               + ", Peach Id="          + peachID
+							               + ", Peach Result Code=" + peachCode
+							               + ", Ret="               + ret.ToString()
+							               + ", SQL="               + sql;
+							Tools.LogException("RegisterThreeD.PageLoad/20",sql);
+							Tools.LogInfo     ("RegisterThreeD.PageLoad/25",sql,222);
 						}
+					}
 					else
 					{
-						lbl100503.Text = "Oops ...";
-						lbl100504.Text = "Something seems to have gone wrong with your payment.<br /><br />We have logged the error and will investigate";
-						sql            =   "Contract Code="     + transRef
-						               + ", Peach Id="          + peachID
-						               + ", Peach Result Code=" + peachCode
-						               + ", Ret="               + ret.ToString()
-						               + ", SQL="               + sql;
-						Tools.LogException("RegisterThreeD.PageLoad/4",sql);
-						Tools.LogInfo     ("RegisterThreeD.PageLoad/5",sql,222);
+						Tools.LogException("RegisterThreeD.PageLoad/30","Ret="+ret.ToString()+" ("+sql+")");
+						Tools.LogInfo     ("RegisterThreeD.PageLoad/35","Ret="+ret.ToString()+" ("+sql+")",222);
 					}
 				}
+			}
+			catch (Exception ex)
+			{
+				Tools.LogException("RegisterThreeD.PageLoad/90","",ex);
+				Tools.LogInfo     ("RegisterThreeD.PageLoad/95",ex.Message,222);
 			}
 		}
 	}
