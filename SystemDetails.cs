577bcd188091b33427283f28d59cf6c98b011687
Paul Kilfoil <paulkilfoil@outlook.com>
2021-06-01 16:26:04 +0200

    Delete SystemDetails.cs

--- SystemDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PCIWebFinAid
-{
-	public static class SystemDetails
-	{
-		public static string AppName    = "Prosperian BackOffice";
-		public static string AppVersion = "1.0.112";
-		public static string AppDate    = "2021/06/01 16:09:33";
-	}
-}
