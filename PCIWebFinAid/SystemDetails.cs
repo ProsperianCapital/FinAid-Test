f36ac015f6eceb253bce573d13b2deb2b5a13aa9
Paul Kilfoil <paulkilfoil@outlook.com>
2021-06-02 11:02:45 +0200

    Add files via upload

--- PCIWebFinAid/SystemDetails.cs
+++ PCIWebFinAid/SystemDetails.cs
@@ -3,7 +3,7 @@
 	public static class SystemDetails
 	{
 		public static string AppName    = "Prosperian BackOffice";
-		public static string AppVersion = "1.0.112";
-		public static string AppDate    = "2021/06/01 16:09:33";
+		public static string AppVersion = "1.0.113";
+		public static string AppDate    = "2021/06/02 10:54:33";
 	}
 }
