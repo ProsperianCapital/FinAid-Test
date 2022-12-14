8f53cb5566426ad8ff91e7f3820e533c5ce44295
Paul Kilfoil <paulkilfoil@outlook.com>
2022-12-14 16:11:34 +0200

    Add files via upload

---
+++ Home.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using PCIBusiness;
+
+namespace PCIWebFinAid
+{
+	public partial class Home : BasePage
+	{
+		protected override void PageLoad(object sender, EventArgs e) // AutoEventWireup = false
+		{
+			byte   err = 0;
+			string url = Request.Url.AbsoluteUri.Trim();
+
+		//	The above is the best one to use
+		//	string urlT1 = Tools.ObjectToString(Request.UrlReferrer);
+		//	string urlT2 = Tools.ObjectToString(Request.Headers["Referer"]);
+		//	string urlT3 = Request.Url.AbsolutePath;
+		//	string urlT4 = Request.Url.OriginalString;
+
+			try
+			{
+				int    k         = url.IndexOf("://");
+				string dName     = url.Substring(k+3);
+				string goTo      = "pgLogon.aspx";
+				string parms     = "";
+				string appStatus = "*";
+				ApplicationCode  = "";
+
+				k = dName.ToUpper().IndexOf("/HOME.ASPX");
+				if ( k > 0 )
+					dName = dName.Substring(0,k);
+				k = url.IndexOf("?");
+				if ( k > 0 )
+					parms = url.Substring(k);
+				string sql = "exec sp_Get_BackOfficeApplication " + Tools.DBString(dName);
+
+				using (MiscList mList = new MiscList())
+					if ( mList.ExecQuery(sql,0) == 0 )
+					{
+						string appSecurity = mList.GetColumn("EnforceMenuItemSecurity");
+						appStatus          = mList.GetColumn("ApplicationStatus").ToUpper();
+						ApplicationCode    = mList.GetColumn("ApplicationCode");
+
+						if ( appStatus != "A" )
+							err  = 10;
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.Registration)     ) // 000
+							goTo = "Register3.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.CRM)              ) // 002
+							goTo = "pgLogonCRM.aspx";
+//						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.Mobile)           ) // 006
+//							goTo = "pgLogonCRM.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.CareAssist)       ) // 100
+							goTo = "CAHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.iSOS)             ) // 110
+							goTo = "ISHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.LifeGuru)         ) // 120
+							goTo = "LGHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.PayPayYa)         ) // 130
+							goTo = "PYHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.AdvantageCard)    ) // 140
+							goTo = "ADVHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.AdvantageCardCRM) )
+							goTo = "ADVCRM.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.SmartStox)        ) // 160
+							goTo = "PCISSHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.SmartStoxCRM)     )
+							goTo = "SSCRM.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.RewardsVault)     ) // 170
+							goTo = "RVHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.RewardsVaultCRM)  )
+							goTo = "RVCRM.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.TankwaCyber)      ) // 180
+							goTo = "TCFHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.TankwaCyberCRM)   )
+							goTo = "TCFCRM.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.ProsperianSTO)    ) // 190
+							goTo = "PCISTOHome.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.ProsperianSTOCRM) )
+							goTo = "PCISTOCRM.aspx";
+						else if ( ApplicationCode == Tools.SystemCode(Constants.ApplicationCode.SmartMoney)       ) // 200
+							goTo = "SMHome.aspx";
+						else
+							err  = 20;
+					}
+					else
+						err =  0;
+					//	err = 30;
+
+				if ( err > 0 )
+					Tools.LogInfo("Home.PageLoad/1","err="       + err.ToString()
+					                            + ", url="       + url
+					                            + ", domain="    + dName
+					                            + ", goTo="      + goTo
+					                            + ", parms="     + parms
+					                            + ", appCode="   + ApplicationCode
+					                            + ", appStatus=" + appStatus, 222);
+
+				Response.Redirect(goTo+parms);
+			}	
+			catch (System.Threading.ThreadAbortException)
+			{
+			//	Ignore
+			}
+			catch (Exception ex)
+			{
+				err = 90;
+				Tools.LogException("Home.PageLoad/2","url="+url,ex);
+			}
+
+			if ( err > 0 )
+				Tools.LogInfo("Home.PageLoad/3","err="     + err.ToString()
+				                            + ", url="     + url
+				                            + ", appCode=" + ApplicationCode, 222);
+		}
+	}
+}
---
+++ SystemDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace PCIWebFinAid
+{
+	public static class SystemDetails
+	{
+		public static string AppName    = "Prosperian BackOffice";
+		public static string AppVersion = "1.0.153";
+		public static string AppDate    = "2022/12/14 15:01:47";
+	}
+}
